4ab24c3725ef305a04e5ce74499e79e27e8c0ca4
NNc31 <Nefyodov.Nazar@outlook.com>
2022-06-26 15:47:09 +0300

    Initial commit

---
+++ src/main/java/ua/edu/sumdu/nefodov/sheltered/ShelteredApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.nefodov.sheltered;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelteredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShelteredApplication.class, args);
+    }
+
+}
