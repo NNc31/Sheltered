8f6e3edbbcb7bd0ebd9a8d9d995ee9bdab7fb7ea
NNc31 <nefodov.nazar@proton.me>
2025-05-10 19:51:07 +0300

    Removed unused redirects

--- web-service/src/main/java/com/nefodov/sheltered/webservice/controller/ShelterWebController.java
+++ web-service/src/main/java/com/nefodov/sheltered/webservice/controller/ShelterWebController.java
@@ -3,7 +3,6 @@ package com.nefodov.sheltered.webservice.controller;
 import com.nefodov.sheltered.shared.model.ShelterCondition;
 import com.nefodov.sheltered.shared.model.ShelterDTO;
 import com.nefodov.sheltered.shared.model.ShelterStatus;
-import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -15,8 +14,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -78,21 +75,15 @@ public class ShelterWebController {
 
 
     @PostMapping("/add")
-    public RedirectView addShelterSubmit(@Valid @ModelAttribute ShelterDTO shelter, HttpSession session, RedirectAttributes redirectAttributes) {
-        String token = (String) session.getAttribute("token");
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + token);
-        HttpEntity<ShelterDTO> entity = new HttpEntity<>(shelter, headers);
-
+    public String addShelterSubmit(@Valid @ModelAttribute ShelterDTO shelter, Model model) {
+        HttpEntity<ShelterDTO> entity = new HttpEntity<>(shelter);
         ResponseEntity<ShelterDTO> response = restTemplate.exchange(
                 "http://api-gateway:8080/shelter-service/add",
                 HttpMethod.POST,
                 entity,
                 ShelterDTO.class
         );
-
-        redirectAttributes.addFlashAttribute("success", "Сховище успішно додано");
-        return new RedirectView("/web-service/shelter/add-form", true);
+        return index(model);
     }
 
     @GetMapping("/edit-form")
@@ -106,11 +97,8 @@ public class ShelterWebController {
     }
 
     @PostMapping("/edit")
-    public RedirectView editShelterSubmit(@Valid @ModelAttribute ShelterDTO shelter, HttpSession session, RedirectAttributes redirectAttributes) {
-        String token = (String) session.getAttribute("token");
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + token);
-        HttpEntity<ShelterDTO> entity = new HttpEntity<>(shelter, headers);
+    public String editShelterSubmit(@Valid @ModelAttribute ShelterDTO shelter, Model model) {
+        HttpEntity<ShelterDTO> entity = new HttpEntity<>(shelter);
 
         ResponseEntity<ShelterDTO> response = restTemplate.exchange(
                 "http://api-gateway:8080/shelter-service/update",
@@ -118,11 +106,7 @@ public class ShelterWebController {
                 entity,
                 ShelterDTO.class
         );
-
-        redirectAttributes.addFlashAttribute("success", "Сховище успішно змінено");
-        //redirectAttributes.addFlashAttribute("error", "Неможливо змінити сховище");
-
-        return new RedirectView("/web-service/shelter/edit-form", true);
+        return index(model);
     }
 
     @GetMapping("/delete-form")
@@ -136,27 +120,18 @@ public class ShelterWebController {
     }
 
     @PostMapping("/delete")
-    public RedirectView deleteShelterSubmit(@RequestParam(value = "lat", required = false) Double lat,
+    public String deleteShelterSubmit(@RequestParam(value = "lat", required = false) Double lat,
                                             @RequestParam(value = "lng", required = false) Double lng,
-                                            HttpSession session,
-                                            RedirectAttributes redirectAttributes) {
-
-        if (lat == null || lng == null) {
-            redirectAttributes.addFlashAttribute("error", "Неможливо видалити сховище");
-        } else {
-            String token = (String) session.getAttribute("token");
+                                      Model model) {
+        if (lat != null && lng != null) {
             HttpHeaders headers = new HttpHeaders();
-            headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + token);
-
             ResponseEntity<Void> response = restTemplate.exchange(
                     "http://api-gateway:8080/shelter-service/delete?lat=" + lat + "&lng=" + lng,
                     HttpMethod.DELETE,
                     new HttpEntity<>(headers),
                     Void.class
             );
-
-            redirectAttributes.addFlashAttribute("success", "Сховище успішно видалено");
         }
-        return new RedirectView("/web-service/shelter/delete-form", true);
+        return index(model);
     }
 }
